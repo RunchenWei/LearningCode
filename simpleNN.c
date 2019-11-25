4f799878e25ec5f606a12e208b62e40c696bc366
RunchenWei <53344105+RunchenWei@users.noreply.github.com>
2019-11-25 22:10:57 +0800

    Right Graph NN (Format)

--- simpleNN.c
+++ simpleNN.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <windows.h>
 
-
 struct Node
 {
     int status;
@@ -10,83 +9,79 @@ struct Node
     int root;
 };
 
-
 int main()
 {
-    int n,p;
-    scanf("%d %d",&n,&p);
+    int n, p;
+    scanf("%d %d", &n, &p);
     struct Node n1[n];
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
-        scanf("%d %d",&n1[i].status,&n1[i].threshold);
-        n1[i].leaf=0;
-        if(n1[i].status==0)
+        scanf("%d %d", &n1[i].status, &n1[i].threshold);
+        n1[i].leaf = 0;
+        if (n1[i].status == 0)
         {
-            n1[i].root=0;
+            n1[i].root = 0;
         }
         else
         {
-            n1[i].root=1;
+            n1[i].root = 1;
         }
-        
     }
-    
+
     int edge[n][n];
     for (int j = 0; j < n; j++)
     {
         for (int k = 0; k < n; k++)
         {
-            edge[j][k]=0;
+            edge[j][k] = 0;
         }
     }
 
-    int a,b,c=0;
+    int a, b, c = 0;
     for (int m = 0; m < p; m++)
     {
-        scanf("%d %d %d",&a,&b,&c);
-        edge[a-1][b-1]=c;
-        n1[a-1].leaf=1;
+        scanf("%d %d %d", &a, &b, &c);
+        edge[a - 1][b - 1] = c;
+        n1[a - 1].leaf = 1;
     }
     int sum;
     for (int s = 0; s < n; s++)
     {
-        sum=0;
-        if (n1[s].status==0)
+        sum = 0;
+        if (n1[s].status == 0)
         {
             for (int t = 0; t < n; t++)
             {
-                if (n1[t].status>0)
+                if (n1[t].status > 0)
                 {
-                    sum=sum+edge[t][s]*n1[t].status;
+                    sum = sum + edge[t][s] * n1[t].status;
                     //printf("t:%d sum:%d   ",t,sum);
                 }
             }
             sum = sum - n1[s].threshold;
             //printf("s:%d sum:%d\n",s,sum);
-            n1[s].status=sum;
+            n1[s].status = sum;
         }
     }
-    int flag=0;
+    int flag = 0;
     for (int i = 0; i < n; i++)
     {
-        if ((n1[i].status>0)&&(n1[i].leaf==0)&&(n1[i].root!=1))
+        if ((n1[i].status > 0) && (n1[i].leaf == 0) && (n1[i].root != 1))
         {
-            printf("%d %d\n",i+1,n1[i].status);
-            flag=1;
+            printf("%d %d\n", i + 1, n1[i].status);
+            flag = 1;
         }
-        if ((n1[i].leaf==0)&&(n1[i].root==1))
+        if ((n1[i].leaf == 0) && (n1[i].root == 1))
         {
-            printf("%d %d\n",i+1,n1[i].status);
-            flag=1;
+            printf("%d %d\n", i + 1, n1[i].status);
+            flag = 1;
         }
-        
-        
     }
-    if(flag==0)
+    if (flag == 0)
     {
         printf("NULL\n");
     }
-    
+
     system("pause");
     return 0;
 }
